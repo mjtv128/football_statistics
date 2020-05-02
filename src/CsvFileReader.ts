c49c897c61bb33e6e19f17e8a9a52000937383e9
Michelle Van <mjtvan@gmail.com>
2020-05-02 21:02:53 +0100

    refactor#1 using generics

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,30 +1,29 @@
 import fs from 'fs'
-import {MatchResult} from './MatchResult'
 
-type MatchData = [Date, string, string, number, number, MatchResult, string]
+export abstract class CsvFileReader<T>{
+        data: T[] = [];
 
-export abstract class CsvFileReader<T> {
-    data: T[] = [];
+        constructor(public filename: string) {}
 
-    constructor(public filename: string) {}
+        //implemented by child class
+        //abstract methods
+        abstract mapRow(row: string[]): T;
+        
+        read(): void {
+        this.data = fs
+            .readFileSync(this.filename, {
+            encoding: "utf8",
+            })
+            .split("\n")
 
-    //implemented by child class
-    //abstract methods
-    abstract mapRow(row: string[]): T;
-    
-    read(): void {
-    this.data = fs
-        .readFileSync(this.filename, {
-        encoding: "utf8",
-        })
-        .split("\n")
+            .map((row: string): string[] => {
+            return row.split(",");
+            })
 
-        .map((row: string): string[] => {
-        return row.split(",");
-        })
-
-        .map(this.mapRow);
+            .map(this.mapRow);
+        }
     }
-    }
+
+
 
 
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -2,7 +2,9 @@ import {CsvFileReader} from './CsvFileReader'
 import {dateStringToDate} from './utils'
 import {MatchResult} from './MatchResult'
 
-export class MatchReader extends CsvFileReader{
+type MatchData = [Date, string, string, number, number, MatchResult, string]
+
+export class MatchReader extends CsvFileReader<MatchData>{
     mapRow(row: string[]): MatchData {
         return [
             dateStringToDate(row[0]),
@@ -15,3 +17,4 @@ export class MatchReader extends CsvFileReader{
         ];
     }
 }
+
